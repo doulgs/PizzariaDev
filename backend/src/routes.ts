a505eb5d7362b5efad2eb7e4b6cb0da0709c4505
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-07 22:41:57 -0300

    Tratativas de errors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/test", (req: Request, res: Response) => {
+  // throw new Error("Erro ao fazer essa requisicao");
   return res.json({ nome: "PizzariaDev" });
 });
 
