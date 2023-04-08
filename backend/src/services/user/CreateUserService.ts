987b215d525b2512fbe15574011fd524ff8af5bc
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-08 11:43:44 -0300

    Update CreateUserService.ts
    Cadastrando Usuario.

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface UserResquest {
   name: string;
   email: string;
@@ -6,9 +8,34 @@ interface UserResquest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserResquest) {
-    console.log(name);
+    //Verificar o email
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+    // Verificar se o Email já existe
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (userAlreadyExists) {
+      throw new Error("Email already exists");
+    }
 
-    return { name: name };
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 export { CreateUserService };
