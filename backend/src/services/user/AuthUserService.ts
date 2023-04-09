947150b6a7675f19ef0a1eead184782f4186b49f
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-09 08:52:26 -0300

    Logando e gerando Token JWT
    Logando e gerando Token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -23,7 +24,24 @@ class AuthUserService {
     }
 
     //Gerar Token JWT = JSON WEB TOKEN
-    return { ok: true };
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
