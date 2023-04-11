c70b2485faa714e9e86a32fd1a1e23a9361f7deb
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-11 20:56:12 -0300

    Func Create/Remove Order
    Funcionalidade para criar e para remover Order (Pedidos)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,8 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 
 const router = Router();
 
@@ -47,5 +49,11 @@ router.get(
   isAuthenticated,
   new ListByCategoryController().handle
 );
+
+// -- Rotas de Order
+
+router.post("/order", isAuthenticated, new CreateOrderController().handle);
+
+router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
 export { router };
