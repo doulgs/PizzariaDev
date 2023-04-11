a5fb51b9c52ac57a49fb3742b1b3e329816214aa
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-11 20:28:18 -0300

    Func List Product By Category_id
    Funcionalidade para Listar os produtos de uma categoria através do ID de uma categoria.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,7 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
 
@@ -39,6 +40,12 @@ router.post(
   isAuthenticated,
   upload.single("file"),
   new CreateProductController().handle
+);
+
+router.get(
+  "/category/product",
+  isAuthenticated,
+  new ListByCategoryController().handle
 );
 
 export { router };
