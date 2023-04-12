36df7d6296491e350f4d3aadd23fc3711750f9cd
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-11 21:43:11 -0300

    func remove Product from Order
    funcionalidade para remover um produto de uma mesa(order)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -15,6 +15,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 const router = Router();
 
@@ -58,5 +59,11 @@ router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
+
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 
 export { router };
