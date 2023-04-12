6fd1e8d06dbd0645bc91fe08f03468de93c3f222
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-11 21:59:43 -0300

    func SendOrder
    Funcionalidade para tirar o pedido de rascunho

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,7 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 const router = Router();
 
@@ -65,5 +66,7 @@ router.delete(
   isAuthenticated,
   new RemoveItemController().handle
 );
+
+router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 export { router };
