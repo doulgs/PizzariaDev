12bf8ca55521eedf2a0b4ccaa13310216fbb2586
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-11 22:10:18 -0300

    func ListOrder
    funcionalidade para listar os pedidos que já n estão mais como rascunho(draft)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,6 +17,7 @@ import { RemoveOrderController } from "./controllers/order/RemoveOrderController
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
+import { ListOrdersController } from "./controllers/order/ListOrdersController";
 
 const router = Router();
 
@@ -68,5 +69,7 @@ router.delete(
 );
 
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
+
+router.get("/orders", isAuthenticated, new ListOrdersController().handle);
 
 export { router };
