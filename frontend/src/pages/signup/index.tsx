181558a60c43424adea7f7f80157416aab61df27
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-13 20:06:09 -0300

    Create SignUp --front
    Criando pagina de cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,34 +1,39 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
-import logoImg from "../../public/logo.svg";
 import Image from "next/image";
-import { Input } from "@/components/ui/Input";
-import { Button } from "@/components/ui/Button";
+import styles from "../../styles/home.module.scss";
+import logoImg from "../../../public/logo.svg";
+
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>DevPizza - Faça seu Login</title>
+        <title>Faça seu Cadastro</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="logo" />
 
         <div className={styles.login}>
+          <h1>Criando sua Conta</h1>
+
           <form>
+            <Input placeholder="Digite seu Nome" type="text" />
+
             <Input placeholder="Digite seu Email" type="text" />
 
             <Input placeholder="Digite sua Senha" type="password" />
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link legacyBehavior href="/signup">
-            <a className={styles.text}>Nao possui uma conta? Cadastrar-se!</a>
+          <Link legacyBehavior href="/">
+            <a className={styles.text}>já possui uma conta? Faça Login!</a>
           </Link>
         </div>
       </div>
